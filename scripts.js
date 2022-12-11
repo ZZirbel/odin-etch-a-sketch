a6c90db691f11cd6118f7ed018f01c1ce3d896e9
Zan Zirbel <zan.zirbel@gmail.com>
2022-12-10 20:45:08 -0800

    get js to display grid of divs in board

--- scripts.js
+++ scripts.js
@@ -9,3 +9,24 @@ const addGridSize = function () {
 };
 
 gridSize.addEventListener("input", addGridSize);
+
+const createDivs = function (value) {
+  let board = document.querySelector("#board");
+  for (let i = 0; i < value; i++) {
+    let row = document.createElement("div");
+    row.className = "grid-row";
+    row.style.setProperty("height", `calc(${board.clientHeight} / ${value})`);
+    for (let j = 1; j <= value; j++) {
+      let square = document.createElement("div");
+      square.className = "grid-square";
+      square.style.setProperty(
+        "width",
+        `calc(${board.clientWidth} / ${value})`
+      );
+      row.appendChild(square);
+    }
+    board.appendChild(row);
+  }
+};
+
+gridSize.addEventListener("input", createDivs(gridSize.value));
