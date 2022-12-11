6f29820e7a0a9e49162b8dba4f4b2e59f9d1bff8
Zan Zirbel <zan.zirbel@gmail.com>
2022-12-10 21:49:23 -0800

    add comments and edit createGrid function

--- scripts.js
+++ scripts.js
@@ -1,35 +1,38 @@
-/* Adds changing text for grid sizing */
+/* selectors for grid size slider */
 const gridDimensions = document.querySelector("#grid-dimensions");
 const gridSize = document.querySelector("#grid-slider");
 
+/* displays default grid dimensions above slider on load */
 gridDimensions.textContent = `${gridSize.value} x ${gridSize.value}`;
 
+/* updates grid dimensions above slider */
 const addGridSize = function () {
   gridDimensions.textContent = `${gridSize.value} x ${gridSize.value}`;
 };
 
 gridSize.addEventListener("input", addGridSize);
 
-const createDivs = function (value) {
+/* Adds grid to board */
+function createGrid(value) {
   let board = document.querySelector("#board");
   for (let i = 0; i < value; i++) {
     let column = document.createElement("div");
     column.className = "grid-column";
-    column.style.setProperty(
-      "height",
-      `calc(${board.clientHeight} / ${value})`
-    );
     for (let j = 1; j <= value; j++) {
       let square = document.createElement("div");
       square.className = "grid-square";
       square.style.setProperty(
         "width",
-        `calc(${board.clientWidth} / ${value})`
+        `calc(${board.clientWidth}px / ${value})`
+      );
+      square.style.setProperty(
+        "height",
+        `calc(${board.clientHeight}px / ${value})`
       );
       column.appendChild(square);
     }
     board.appendChild(column);
   }
-};
+}
 
-gridSize.addEventListener("input", createDivs(gridSize.value));
+gridSize.addEventListener("input", createGrid(gridSize.value));
